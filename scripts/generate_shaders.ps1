9e557fe7b022860f52856f5037717dd0478a6f1f
Tomáš Malínský <tmal@malanak.cz>
2026-06-27 11:13:32 +0200

    Texture shader, remade ps1

--- scripts/generate_shaders.ps1
+++ scripts/generate_shaders.ps1
@@ -1,5 +1,28 @@
-mkdir resources/generated/shaders/spirv
-slangc resources/shader/basic.slang `
--no-mangle `
--fvk-use-gl-layout `
--o resources/generated/shaders/spirv/basic.spv
+$shaderDir = "resources/shader"
+$spirvDir  = "resources/generated/shaders/spirv"
+
+if (Test-Path -Path $spirvDir) {
+    Remove-Item -Path $spirvDir -Recurse -Force
+}
+New-Item -Path $spirvDir -ItemType Directory -Force | Out-Null
+
+if (-not (Test-Path -Path $shaderDir)) {
+    Write-Error "Source directory '$shaderDir' does not exist!"
+    exit
+}
+
+Get-ChildItem -Path $shaderDir -File | ForEach-Object {
+    $filename = $_.BaseName
+
+    $inputFile  = $_.FullName
+    $outputFile = "$spirvDir/$filename.spv"
+
+    Write-Host "Compiling: $_.Name -> $filename.spv"
+
+    & slangc $inputFile `
+        -no-mangle `
+        -fvk-use-gl-layout `
+        -o $outputFile
+}
+
+Write-Host "Shader compilation complete!" -ForegroundColor Green
